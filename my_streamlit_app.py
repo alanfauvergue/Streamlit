e09d116f9c81901b99c06a4c1a012fe09b26ac95
alanfauvergue <168745000+alanfauvergue@users.noreply.github.com>
2024-05-23 22:40:38 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,16 +3,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot
 
-st.title('Hello Wilders, welcome to my application!')
+st.title('Hello corrector, welcome to my application wich deals with car analysis !')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("let's see some data about car from different region")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-df_weather
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_car = pd.read_csv(link)
+df_car
 
-df_weather['DATE'] = pd.to_datetime(df_weather['DATE'])
 
-viz_correlation = sns.heatmap(df_weather.loc[:,df_weather.columns != "OPINION"].corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
+viz_correlation = sns.heatmap(df_car.loc[:,df_weather.columns != "continent"].corr(), center=0, annot = True)
 
 st.pyplot(viz_correlation.figure)
+
+st.write("we can see a strong positive correlation beetween cylinders and weightlbs, hp, cubicinches.")
+st.write("there is also a strong negative correlation beetween mpd and weightlbs, cubicinches, cylinders, hp.")
